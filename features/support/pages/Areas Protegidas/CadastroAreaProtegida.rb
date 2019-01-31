e9fad8553f6bb1d4a720f69900f42db47ddde331
duhAmorim <duh-amorim@hotmail.com>
2019-01-31 15:09:43 -0200

    Criação de uma unidade

--- features/support/pages/Areas Protegidas/CadastroAreaProtegida.rb
+++ features/support/pages/Areas Protegidas/CadastroAreaProtegida.rb
@@ -5,14 +5,14 @@ class CadastroAreaProtegida < SitePrism::Page
     element :flagInterna,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_entRblExterna_0'
     element :flagExterna,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_entRblExterna_1'
     element :grupo,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_ddlGrupoUC'
-    element :categoria.'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_ddlCategoriaUC'
+    element :categoria,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_ddlCategoriaUC'
     element :areaProtegida,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_txtNome'
     element :sigla,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_txtSigla'
     element :codigo,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_txtCodigoUnidade'
     element :orgaoGestor,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_ddlOrgaoGestor'
     element :unidadeReponsavel,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_entDpUnidadeResponsabel_txtText_Input'
     element :municipio,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_entDpMunicipio_txtText_Input'
-    element :area:,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_txtAreaUC'
+    element :area,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_txtAreaUC'
     element :descricao,'#ctl00_conteudo_TabNavegacao_TBCadastro_ucCadastro_entTxtDesUnidade'
 
     #btn
